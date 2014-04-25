1d1334409d006fe695d942a9758504f3a14535b3
Ashraf KACIMI <kacimi.achraf@gmail.com>
2014-04-25 21:04:27 +0100

    ajout des regex

--- search.c
+++ search.c
@@ -6,17 +6,18 @@
 #include <string.h>
 #include <time.h>
 #include <math.h>
+#include <regex.h>
 
-/*  DONE:
-    Recherche récursive d'un fichier grace à son nom:
-    Intégration des options
-    Fonction d'affichage des infos d'un fichier (utiliser struct stat):
-    
+
+/* DONE:
+Recherche récursive d'un fichier grace à son nom:
+Intégration des options
+Fonction d'affichage des infos d'un fichier (utiliser struct stat):
+Gerer les options: le répertoire courant
 */
 
 /* TODO LIST:
-        Intégrer les regex
-        Gerer les options: le répertoire courant
+Intégrer les regex
 */
 
 int trouve = 0; // variable globale mise à 1 si on trouve le fichier dans l'arborescence qui descend du path de recherche
@@ -29,7 +30,7 @@ typedef struct options{ // regroupe les options pour réduire le nombre de param
     short date_creation;
 }options;
 
-void init_options(options *opt){    //Initialise les champs d'une struct options
+void init_options(options *opt){ //Initialise les champs d'une struct options
                     opt->date_creation = 0;
                     opt->last_modif = 0;
                     opt->taille = 0;
@@ -37,12 +38,12 @@ void init_options(options *opt){    //Initialise les champs d'une struct options
                     opt->protection = 0;
 }
 
-void show_help(){  //Affiche l'aide sur la commande search
+void show_help(){ //Affiche l'aide sur la commande search
     
-    printf("                        ---------------------                             ");
+    printf("              --------------------- \n");
     printf("\n\nsearch est une commande qui permet de rechercher récursivement des fichiers selon leur nom");
-    printf("\n\n       search [Nom_du_Répertoire] [-option] Nom_du_Fichier");
-    printf("\n\n                    Liste des options:");
+    printf("\n\n search [Nom_du_Répertoire] [-option] Nom_du_Fichier");
+    printf("\n\n Liste des options:");
     printf("\n -d : Afficher la date de création et dernière utilisation du fichier");
     printf("\n -m : Afficher la date de la dernière modification du fichier");
     printf("\n -d : Afficher la taille du fichier");
@@ -50,17 +51,17 @@ void show_help(){  //Affiche l'aide sur la commande search
     printf("\n -p : Afficher la protection du fichier");
     printf("\n -a : Afficher toutes les caracteristiques ci dessus");
     printf("\n -n : Parcourir n niveaux, avec n un entier positif\n\n");
-    printf("                        ---------------------                             \n");
+    printf("                --------------------- \n");
 }
 
 char type_fichier(struct stat status) {
-        // Retourne un caractère suivant le type  du fichier
-    if  (S_ISREG(status.st_mode)) return '-';
+        // Retourne un caractère suivant le type du fichier
+    if (S_ISREG(status.st_mode)) return '-';
     if (S_ISFIFO(status.st_mode)) return 'p';
-    if  (S_ISCHR(status.st_mode)) return 'c';
-    if  (S_ISBLK(status.st_mode)) return 'b';
-    if  (S_ISDIR(status.st_mode)) return 'd';
-    if  (S_ISLNK(status.st_mode)) return 'l';
+    if (S_ISCHR(status.st_mode)) return 'c';
+    if (S_ISBLK(status.st_mode)) return 'b';
+    if (S_ISDIR(status.st_mode)) return 'd';
+    if (S_ISLNK(status.st_mode)) return 'l';
     if (S_ISSOCK(status.st_mode)) return 's';
 }
 
@@ -68,16 +69,16 @@ int is_num(char c){
     return ((c > 47) && (c < 58));
 }
 
-/*  FONCTION D'AFFICHAGE TODO*/
-// droits, 
+/* FONCTION D'AFFICHAGE TODO*/
+// droits,
 void afficher_infos(const char * filename, struct stat status, options opt ){
     char* access = NULL;
     char* creation = NULL;
-    char* modification = NULL; 
+    char* modification = NULL;
     if (opt.type){
         printf("%c", type_fichier(status));
     }
-    if (opt.protection){   
+    if (opt.protection){
         char droits[]="---------";
         if (status.st_mode & S_IRUSR)// GRP OTH
             droits[0] = 'r';
@@ -104,8 +105,8 @@ void afficher_infos(const char * filename, struct stat status, options opt ){
     if (opt.date_creation){
         access = ctime(&status.st_atime);
         creation = ctime(&status.st_ctime);
-        access[strlen(access)-1] = '\0';
-        creation[strlen(access)-1] = '\0';
+        access[strlen(access) - 1] = '\0';
+        creation[strlen(creation) - 1] = '\0';
         printf("%s %s ", creation, access);
     }
     if (opt.last_modif){
@@ -123,14 +124,14 @@ short get_option(char* option, options *opt, int *profondeur){
     int i = 1, k = 0;
     char str_profondeur[10];
 
-    if  (option== NULL)  //
-            return 1;    //
+    if (option== NULL) //
+            return 1; //
         else if (!strcmp(option, "--help")){
             show_help();
         }
         else if (option[0] != '-')
                 return 0;
-            else{               //
+            else{ //
                     if (strstr(option, "a") != NULL){
                         opt->date_creation = 1;
                         opt->last_modif = 1;
@@ -165,30 +166,35 @@ short get_option(char* option, options *opt, int *profondeur){
         return 1;
 }
 
-void search(const char * path, const char * nom_fichier, options opt, int nb_niveaux){
+int matches(const char *str, regex_t regex){
+    return  !regexec (&regex, str, 0, NULL, 0);
+}
+
+void search(const char * path, regex_t nom_fichier, options opt, int nb_niveaux){
 // Rôle : traverse le répertoire et, éventuellement, ses sous-répertoires à la recherche du fichier de nom nom_fichier, avec des options
         if (nb_niveaux < 1)
-            return; 
+            return;
         // --------- déclaration des variables ------------//
         struct dirent * entry = NULL; //simule un fichier lu ('dirent' pour DIRectory ENTity)
-        struct stat status;           // structure utilisée pour récupérer les informations stockées dans un inode
-        char filename [255];          // sert de path absolu pour explorer les fichiers du répertoire
-        DIR *dir = NULL;              //simule un répertoire
+        struct stat status; // structure utilisée pour récupérer les informations stockées dans un inode
+        char filename [256]; // sert de path absolu pour explorer les fichiers du répertoire
+        DIR *dir = NULL; //simule un répertoire
         
         // ----------- phase de recherche -------- //
         if ( (dir = opendir(path)) == NULL){
-            printf("\nLe chemin que vous avez spécifié n'existe pas\n");
-            return;   
+            return;
         }
 
-
-        while ((entry = readdir(dir)) != NULL) {               //readdir(*DIR) retourne un 'dirent' s'il y a encore des fichiers à lire, NULL sinon
+        while ((entry = readdir(dir)) != NULL) { //readdir(*DIR) retourne un 'dirent' s'il y a encore des fichiers à lire, NULL sinon
+            if (entry -> d_type ==  DT_LNK)
+                return;
             sprintf(filename, "%s/%s", path, entry -> d_name); // donne 'path'/'nom_du_f'
-            stat(filename, &status);                           // mettre les infos du fichier de path absolu filename dans la struct status
+            stat(filename, &status); // mettre les infos du fichier de path absolu filename dans la struct status
             /* cas d'un répertoire */
+            
             if ( S_ISDIR(status.st_mode)) {
                 
-                if (strcmp(entry -> d_name, ".") && strcmp(entry -> d_name, ".."))  // on doit filtrer . et .. !
+                if (strcmp(entry -> d_name, ".") && strcmp(entry -> d_name, "..")) // on doit filtrer . et .. !
                     // recursivité FTW ! *_*
                     search(filename, nom_fichier, opt, nb_niveaux - 1);
                 }
@@ -196,7 +202,7 @@ void search(const char * path, const char * nom_fichier, options opt, int nb_niv
             /* cas d'un fichier */
             else {
                     // On récupère le nom du fichier avec entry->d_name et on compare avec le nom du fichier qu'on cherche
-                    if (!strcmp(entry->d_name, nom_fichier )){ // si nom_fichier == le fichier courant
+                    if (matches(entry->d_name, nom_fichier)){ // si nom_fichier == le fichier courant
                         if (!trouve) printf("Le fichier existe au(x) chemin(s) suivant(s): \n"); // affichée au max une fois
                         afficher_infos(filename, status, opt);
                         //printf("%s %c %s \n", ctime(&status.st_mtime),type_fichier(status), path);
@@ -205,25 +211,41 @@ void search(const char * path, const char * nom_fichier, options opt, int nb_niv
                     }
                 continue;
                 }
+
         }
 
         closedir(dir); // fermer le répertoire
-        
-         
-        
     }
 
 
 void main(int argc, char** argv){
-    trouve = 0;
-    int i = 0, 
-        profondeur = (int)INFINITY;
+    int i = 1;
+    int profondeur = (int) INFINITY;
     options opt;
-    char* s = getwd(NULL);
+    char path [256];
+    regex_t regex ;
+    
+    trouve = 0;
+    if ((argc == 2) || argv[1][0] == '-'){
+        getcwd(path, sizeof(path));
+        i = 0;   
+    }
+    else{
+        sprintf(path , "%s", argv[1]);
+    }
+    printf("%s\n", path);
     init_options(&opt);
-    for (i = 2; i < argc; i++)
+    while (i < argc-1){
         get_option(argv[i], &opt, &profondeur);
-    search(argv[1], argv[argc-1], opt, profondeur);  //respectivement : path nom_fichier options
-    if (!trouve) 
-        printf("Le fichier que vous avez recherché n'existe pas au path spécifié\n"); 
+        i++;
+    }
+    if (opendir(path) == NULL){
+        printf("Le chemin que vous avez entré n'existe pas\n");
+        return;
+    }
+    regcomp (&regex, argv[argc-1], REG_NOSUB | REG_EXTENDED);
+    search(path, regex, opt, profondeur); //respectivement : path nom_fichier options
+    
+    if (!trouve)
+        printf("Le fichier que vous avez recherché n'existe pas au path spécifié\n");
 }
